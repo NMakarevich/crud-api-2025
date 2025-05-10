58dc6ecff3562b58cbfab080c017868b64308fc6
NMakarevich <nikoladze529@mail.ru>
2025-05-11 00:23:29 +0300

    fix: fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
 import eslint from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
 
 
 export default tseslint.config({
-    files: ["**/*.{js,mjs,cjs,ts}"],
     plugins: { eslint, tseslint },
     extends: [
         tseslint.configs.recommended,
@@ -16,8 +14,11 @@ export default tseslint.config({
       "@typescript-eslint/no-unused-vars": "error"
     },
     languageOptions: {
-      globals: globals.node,
+      globals: {
+          ...globals.node,
+          ...globals.jest
+      },
       parser: tseslint.parser,
-    }
+    },
   }
 );
